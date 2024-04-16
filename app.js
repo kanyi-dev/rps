fb006b3c3cab85f5bee029e2a2b01977d5eebbce
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-04-16 12:42:16 +0300

    create playround code (incomplete)

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 const getComputerChoice = () => {
-  return Math.floor(Math.random() * 3) + 1;
+  let words = ["rock", "paper", "scissors"];
+  let rand = Math.floor(Math.random() * words.length);
+  return words[rand];
 };
+
+console.log(getComputerChoice())
+
+const playRound = (playerSelection, computerSelection) => {
+    let result;
+    let computerScore;
+    let playerScore;
+    let playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection){
+        result = "It's a draw!!";
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        result = "You Win! Rock breaks Scissors!";
+        playerScore += 1;
+    } else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        result = "You lose! Paper covers rock!";
+        computerScore += 1;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        result = "You Win! Paper covers Rock!";
+        playerScore += 1;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        result = "You Win! Paper cuts Scissors"
+    }
+}
