fba84500b02d1eab794735a149fab962aac45f51
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-08-06 15:37:39 +0300

    logic for computer choice

---
+++ app.js
@@ -0,0 +1,10 @@
+const getComputerChoice = () => {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) {
+    return "Rock";
+  } else if (choice === 1) {
+    return "Paper";
+  } else if (choice === 2) {
+    return "Scissors";
+  }
+};
